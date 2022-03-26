ce140cadc45bc76ba568008d693a88976d258983
Le-Fab <kouonang2002@gmail.com>
2022-03-26 18:43:42 +0100

    les fichiers à jours

---
+++ functions.js
@@ -0,0 +1,22 @@
+export const divisorsOfNumbers = (number) => {
+  let diviseurs = [];
+  if (number < 2) return (diviseurs = [1]);
+  for (let i = 1; i <= number; i++) {
+    if (number % i == 0) diviseurs.push(i);
+  }
+  return diviseurs;
+};
+export const primeNumbers = (number) => {
+  let isprime = true;
+//au depart est true pour tous
+  if (number > 0 && number < 2) return !isprime;
+  if (number == 2) return isprime;
+  if (number > 2) {
+   // console.log(!isprime);
+    for (let i = 2; i <= Math.ceil(number / 2); i++) {
+     // console.log(Math.ceil(number / 2));
+      if (number % i == 0) return !isprime;
+    }
+    return isprime;
+  }
+};
