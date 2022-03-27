192adf3778cba966449b695e6f51c0909ed676d6
Le-Fab <kouonang2002@gmail.com>
2022-03-27 12:27:39 +0100

    ajout de la fonction parité

--- functions.js
+++ functions.js
@@ -8,15 +8,19 @@ export const divisorsOfNumbers = (number) => {
 };
 export const primeNumbers = (number) => {
   let isprime = true;
-//au depart est true pour tous
+  //au depart est true pour tous
   if (number > 0 && number < 2) return !isprime;
   if (number == 2) return isprime;
   if (number > 2) {
-   // console.log(!isprime);
+    // console.log(!isprime);
     for (let i = 2; i <= Math.ceil(number / 2); i++) {
-     // console.log(Math.ceil(number / 2));
+      // console.log(Math.ceil(number / 2));
       if (number % i == 0) return !isprime;
     }
     return isprime;
   }
+};
+export const evenNumbers = (number) => {
+  if (number % 2 == 0) return true;
+  return false;
 };
